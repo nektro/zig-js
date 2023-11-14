690223fddfe79781a38167b4d35397b24cb489b0
Meghan Denny <hello@nektro.net>
2023-11-13 19:17:07 -0800

    add more standard build options

--- build.zig
+++ build.zig
@@ -2,14 +2,18 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
+    const strip = b.option(bool, "strip", "Strip debug symbols") orelse false;
+    const pie = b.option(bool, "pie", "Build a position independent executable.") orelse false;
 
     const exe = b.addExecutable(.{
         .name = "hubscher",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
-        .optimize = optimize,
+        .optimize = mode,
     });
+    exe.strip = strip;
+    exe.pie = pie;
 
     b.installArtifact(exe);
 
@@ -27,8 +31,10 @@ pub fn build(b: *std.Build) void {
     const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
-        .optimize = optimize,
+        .optimize = mode,
     });
+    unit_tests.strip = strip;
+    unit_tests.pie = pie;
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
